0cefe90a5106c4c8f36699a745d4afe6779d8191
unknown <sutharhardik747@gmail.com>
2023-07-07 12:39:28 +0530

    project setup

---
+++ src/Button.jsx
@@ -0,0 +1,11 @@
+import { styled } from "styled-components";
+
+export const Button = styled.button`
+  background-color: var(--color-brand-200);
+  padding: 10px;
+  cursor: pointer;
+  &:hover {
+    color: aquamarine;
+    padding: 20px;
+  }
+`;
